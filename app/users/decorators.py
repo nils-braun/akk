afb45e566ce86e2821a97e1f94579f079b5e6b56
Nils <area51.nils@googlemail.com>
2016-07-26 21:19:23 +0200

    Added templates from tutorial

---
+++ app/users/decorators.py
@@ -0,0 +1,17 @@
+from functools import wraps
+
+from flask import g, flash, redirect, url_for, request
+
+
+def requires_login(f):
+    """
+    Function decorator to add a required logged in user to a flask route.
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if g.user is None:
+            flash(u'You need to be signed in for this page.')
+            return redirect(url_for('users.login', next=request.path))
+        return f(*args, **kwargs)
+
+    return decorated_function
