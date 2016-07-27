621ad92ad4c9f67802b1f438699ea99d5da0154a
Nils <area51.nils@googlemail.com>
2016-07-27 17:58:11 +0200

    Made it easier to create the db in the terminal.

--- shell.py
+++ shell.py
@@ -6,4 +6,12 @@ from pprint import pprint
 from flask import *
 from app import *
 
+
+def create_db():
+    """
+    Convenience function to create the db file with all needed tables.
+    """
+    db.create_all()
+
+print("Type create_db() to create the DB.")
 os.environ['PYTHONINSPECT'] = 'True'
