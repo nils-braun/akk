a6504bf90eb035dbed0962faa211ad6e61546d9b
Nils <area51.nils@googlemail.com>
2016-07-27 20:49:46 +0200

    Moved some convenience functions out of the views python script.

---
+++ app/songs/functions.py
@@ -0,0 +1,69 @@
+from app import db
+from app.songs.models import Song, Dance, Artist
+from flask import request, flash, redirect, url_for, render_template
+
+
+def delete_entity(FormClass, DataClass, name, song_argument):
+    form = FormClass(request.form)
+    data_to_delete = []
+    if form.validate_on_submit():
+        entity = DataClass.query.filter_by(name=form.name.data).first()
+        if entity:
+            filter_dict={song_argument: entity.id}
+            data_to_delete = Song.query.filter_by(**filter_dict).all()
+
+            if form.sure_to_delete.data:
+
+                old_artists_and_dances = [(song.artist, song.dance) for song in data_to_delete]
+
+                db.session.delete(entity)
+                db.session.commit()
+
+                for artist, dance in old_artists_and_dances:
+                    delete_unused_old_entities(artist, dance)
+
+                flash('Sucessfully deleted %s' % entity.name)
+                return redirect(url_for('songs.home'))
+            else:
+                flash('Are you sure you want to delete?')
+                form.sure_to_delete.data = True
+
+        else:
+            flash('No %s with this name' % name, 'error-message')
+    return render_template("songs/deletion_form.html", form=form, data_to_delete=data_to_delete)
+
+
+def delete_unused_old_entities(old_artist, old_dance):
+    if Song.query.filter_by(artist_id=old_artist.id).count() == 0:
+        db.session.delete(old_artist)
+        db.session.commit()
+
+        flash('Deleted artist {} because no song is related any more.'.format(old_artist.name))
+
+    if Song.query.filter_by(dance_id=old_dance.id).count() == 0:
+        db.session.delete(old_dance)
+        db.session.commit()
+
+        flash('Deleted dance {} because no song is related any more.'.format(old_dance.name))
+
+
+def get_or_add_artist_and_dance(form):
+    """
+    Get the artist and the dance with the names form the form from the db.
+    If they are not present, create new ones.
+    """
+    dance = Dance.query.filter_by(name=form.dance_name.data).first()
+    if not dance:
+        dance = Dance(name=form.dance_name.data)
+        db.session.add(dance)
+        db.session.commit()
+
+        flash('No dance with the name {}. Added new.'.format(dance.name))
+    artist = Artist.query.filter_by(name=form.artist_name.data).first()
+    if not artist:
+        artist = Artist(name=form.artist_name.data)
+        db.session.add(artist)
+        db.session.commit()
+
+        flash('No artist with the name {}. Added new.'.format(artist.name))
+    return artist, dance
--- app/songs/views.py
+++ app/songs/views.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
-
 from app import db
 from app.songs.forms import CreateSongForm, DeleteArtistForm, DeleteDanceForm, EditSongForm
+from app.songs.functions import delete_entity, delete_unused_old_entities, get_or_add_artist_and_dance
 from app.songs.models import Artist, Dance, Song
-from app.users.models import User
 from app.users.decorators import requires_login
+from app.users.models import User
+from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 
 mod = Blueprint('songs', __name__, url_prefix='/songs')
 
@@ -44,36 +44,6 @@ def delete_dance():
     return delete_entity(DeleteDanceForm, Dance, "dance", "dance_id")
 
 
-def delete_entity(FormClass, DataClass, name, song_argument):
-    form = FormClass(request.form)
-    data_to_delete = []
-    if form.validate_on_submit():
-        entity = DataClass.query.filter_by(name=form.name.data).first()
-        if entity:
-            filter_dict={song_argument: entity.id}
-            data_to_delete = Song.query.filter_by(**filter_dict).all()
-
-            if form.sure_to_delete.data:
-
-                old_artists_and_dances = [(song.artist, song.dance) for song in data_to_delete]
-
-                db.session.delete(entity)
-                db.session.commit()
-
-                for artist, dance in old_artists_and_dances:
-                    delete_unused_old_entities(artist, dance)
-
-                flash('Sucessfully deleted %s' % entity.name)
-                return redirect(url_for('songs.home'))
-            else:
-                flash('Are you sure you want to delete?')
-                form.sure_to_delete.data = True
-
-        else:
-            flash('No %s with this name' % name, 'error-message')
-    return render_template("songs/deletion_form.html", form=form, data_to_delete=data_to_delete)
-
-
 @mod.route('/create_song/', methods=['GET', 'POST'])
 @requires_login
 def create_song():
@@ -84,7 +54,7 @@ def create_song():
     if form.validate_on_submit():
         artist, dance = get_or_add_artist_and_dance(form)
 
-        song = Song(title=form.title.data, dance=dance, artist=artist)
+        song = Song(title=form.title.data, dance=dance, artist=artist, creation_user=g.user)
         db.session.add(song)
         db.session.commit()
 
@@ -152,37 +122,3 @@ def edit_song():
     return render_template("songs/edit_song.html", form=form)
 
 
-def delete_unused_old_entities(old_artist, old_dance):
-    if Song.query.filter_by(artist_id=old_artist.id).count() == 0:
-        db.session.delete(old_artist)
-        db.session.commit()
-
-        flash('Deleted artist {} because no song is related any more.'.format(old_artist.name))
-
-    if Song.query.filter_by(dance_id=old_dance.id).count() == 0:
-        db.session.delete(old_dance)
-        db.session.commit()
-
-        flash('Deleted dance {} because no song is related any more.'.format(old_dance.name))
-
-
-def get_or_add_artist_and_dance(form):
-    """
-    Get the artist and the dance with the names form the form from the db.
-    If they are not present, create new ones.
-    """
-    dance = Dance.query.filter_by(name=form.dance_name.data).first()
-    if not dance:
-        dance = Dance(name=form.dance_name.data)
-        db.session.add(dance)
-        db.session.commit()
-
-        flash('No dance with the name {}. Added new.'.format(dance.name))
-    artist = Artist.query.filter_by(name=form.artist_name.data).first()
-    if not artist:
-        artist = Artist(name=form.artist_name.data)
-        db.session.add(artist)
-        db.session.commit()
-
-        flash('No artist with the name {}. Added new.'.format(artist.name))
-    return artist, dance
