7694d3fe903bbf879106e98d712ff4102bd18b9f
Nils <area51.nils@googlemail.com>
2016-07-27 19:45:22 +0200

    Fixed errors in the songs module.

--- app/songs/views.py
+++ app/songs/views.py
@@ -9,11 +9,11 @@ from app.users.decorators import requires_login
 mod = Blueprint('songs', __name__, url_prefix='/songs')
 
 
-@mod.route('/me/')
+@mod.route('/home/')
 @requires_login
 def home():
-    raise NotImplementedError()
-    #return render_template("songs/list.html", user=g.user)
+    songs = Song.query.all()
+    return render_template("songs/list.html", user=g.user, songs=songs)
 
 
 @mod.before_request
@@ -88,47 +88,70 @@ def edit_song():
     """
     Edit or delete a song
     """
-
     form = EditSongForm(request.form)
 
-    if not form.song_id:
-        # it seems we are coming directly from the main page
-        song_id = request.args.get("song_id")
-        song = Song.query.filter_by(id=song_id).first()
-
-        form.song_id = song.id
-        form.artist_name.data = song.artist.name
-        form.dance_name.data = song.dance.name
-
-        if not song:
-            return render_template("404.html")
-
-    else:
-        # this will be called on reloading the form
-        song_id = form.song_id
-        song = Song.query.filter_by(id=song_id).first()
-
     if form.validate_on_submit():
+        # this will be called on reloading the form
+        song_id = form.song_id.data
+        song = Song.query.filter_by(id=song_id).first()
+
+        old_artist = song.artist
+        old_dance = song.dance
+
         if form.edit_button.data:
+
             artist, dance = get_or_add_artist_and_dance(form)
 
             song.artist_id = artist.id
             song.dance_id = dance.id
             song.title = form.title.data
 
-            db.session.update(g.song)
+            db.session.merge(song)
             db.session.commit()
+
+            if artist != old_artist or dance != old_dance:
+                delete_unused_old_entities(old_artist, old_dance)
 
             flash('Sucessfully updated song')
         elif form.delete_button.data:
             db.session.delete(song)
             db.session.commit()
 
+            delete_unused_old_entities(old_artist, old_dance)
+
             flash('Sucessfully deleted song')
 
         return redirect(url_for('songs.home'))
 
+    else:
+        # it seems we are coming directly from the main page
+        song_id = request.args.get("song_id")
+
+        song = Song.query.filter_by(id=song_id).first()
+
+        if not song:
+            return render_template("404.html")
+
+        form.song_id.data = song_id
+        form.title.data = song.title
+        form.artist_name.data = song.artist.name
+        form.dance_name.data = song.dance.name
+
     return render_template("songs/edit_song.html", form=form)
+
+
+def delete_unused_old_entities(old_artist, old_dance):
+    if Song.query.filter_by(artist_id=old_artist.id).count() == 0:
+        db.session.delete(old_artist)
+        db.session.commit()
+
+        flash('Deleted artist {} because no song is related any more.'.format(old_artist.name))
+
+    if Song.query.filter_by(dance_id=old_dance.id).count() == 0:
+        db.session.delete(old_dance)
+        db.session.commit()
+
+        flash('Deleted dance {} because no song is related any more.'.format(old_dance.name))
 
 
 def get_or_add_artist_and_dance(form):
@@ -138,16 +161,16 @@ def get_or_add_artist_and_dance(form):
     """
     dance = Dance.query.filter_by(name=form.dance_name.data).first()
     if not dance:
-        flash('No dance with this name. Added new.')
-
         dance = Dance(name=form.dance_name.data)
         db.session.add(dance)
         db.session.commit()
+
+        flash('No dance with the name {}. Added new.'.format(dance.name))
     artist = Artist.query.filter_by(name=form.artist_name.data).first()
     if not artist:
-        flash('No artist with this name. Added new.')
-
         artist = Artist(name=form.artist_name.data)
         db.session.add(artist)
         db.session.commit()
+
+        flash('No artist with the name {}. Added new.'.format(artist.name))
     return artist, dance
