2ab2cd381b2a5be95ac4d0e80d1fa4f3d061f558
Nils <area51.nils@googlemail.com>
2016-07-27 20:50:58 +0200

    Do also show the new changes and create them in new dbs.

--- shell.py
+++ shell.py
@@ -3,6 +3,8 @@ import os
 import readline
 from pprint import pprint
 
+from app.songs.models import Song, Artist, Dance
+from app.users.models import User
 from flask import *
 from app import *
 
@@ -12,6 +14,21 @@ def create_db():
     Convenience function to create the db file with all needed tables.
     """
     db.create_all()
+
+    user = User("test", "test@test.com", "pbkdf2:sha1:1000$VUu0UWDW$211afd0957df48d23553a119668dbc331b84c8cd")
+    db.session.add(user)
+    db.session.commit()
+
+    artist = Artist("b")
+    db.session.add(artist)
+    db.session.commit()
+
+    dance = Dance("c")
+    db.session.add(dance)
+    db.session.commit()
+
+    db.session.add(Song("a", artist, dance, user))
+    db.session.commit()
 
 print("Type create_db() to create the DB.")
 os.environ['PYTHONINSPECT'] = 'True'
