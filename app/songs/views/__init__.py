166982c3a6cfdeafa04455ef742e81836c0a4324
Nils Braun <nils.braun@kit.edu>
2016-09-04 00:26:55 +0200

    Better make a function to add the views.

--- app/songs/views/__init__.py
+++ app/songs/views/__init__.py
@@ -1,17 +1,8 @@
-from flask import Blueprint, g, session
+from flask import Blueprint
 
-from app.users.models import User
+from app.functions import add_before_request
+from app.songs.views.views import add_song_views
 
 mod = Blueprint('songs', __name__, url_prefix='/songs')
-
-from app.songs.views.views import *
-
-
-@mod.before_request
-def before_request():
-    """
-    pull user's profile from the database before every request are treated
-    """
-    g.user = None
-    if 'user_id' in session:
-        g.user = User.query.get(session['user_id'])
+add_before_request(mod)
+add_song_views(mod)
