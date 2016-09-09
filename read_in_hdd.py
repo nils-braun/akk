10e48f85251bf3ffd6253834fd38ee788c499aa0
Nils Braun <nils.braun@kit.edu>
2016-09-10 00:07:03 +0200

    Automatically add tags when reading in the files.

--- read_in_hdd.py
+++ read_in_hdd.py
@@ -35,6 +35,9 @@ if __name__ == '__main__':
 
     added_counter = 0
 
+    new_label, _ = Label.get_or_add_label("new")
+    probably_wrong_label, _ = Label.get_or_add_label("probably_wrong")
+
     for possible_dance_dir in top_level_entries:
         possible_dance_name = os.path.split(possible_dance_dir)[-1]
 
@@ -52,8 +55,21 @@ if __name__ == '__main__':
                 print("File name {} in wrong format. Skipping.".format(file_name_with_this_dance))
                 continue
             else:
-                dance_name, artist_name, title = dance_artist_title.split(" - ")
+                dance_name, artist_name, title_with_tag = dance_artist_title.split(" - ")
                 artist, artist_new_created = Artist.get_or_add_artist(artist_name)
+
+                labels = []
+
+                if "(" in title_with_tag and title_with_tag[-1] == ")":
+                    title = title_with_tag[:title_with_tag.rfind("(")]
+                    label_name_string = title_with_tag[title_with_tag.rfind("(") + 1:title_with_tag.rfind(")")]
+                    label_names = label_name_string.split(",")
+                    labels = [Label.get_or_add_label(label_name.strip())[0] for label_name in label_names]
+                else:
+                    title = title_with_tag
+
+                if "_" in title or "_" in artist.name or "_" in dance.name:
+                    labels.append(probably_wrong_label)
 
                 new_song = Song()
                 new_song.title = title
@@ -63,8 +79,8 @@ if __name__ == '__main__':
                 new_song.path = file_name_with_this_dance.replace(base_path, "")[1:]
                 new_song.duration = get_song_duration(file_name_with_this_dance)
 
-                new_label, label_new_created = Label.get_or_add_label("new")
-                new_song.labels = [new_label]
+                labels.append(new_label)
+                new_song.labels = labels
 
                 db.session.add(new_song)
 
