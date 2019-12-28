3221f258c02dcea30fca447229f2a4c4ce1593af
Nils Braun <nils.braun@kit.edu>
2019-12-28 10:00:44 +0100

    Use a different read in script

--- read_in_hdd.py
+++ read_in_hdd.py
@@ -13,15 +13,13 @@ if __name__ == '__main__':
     parser.add_argument("user", help="User name to use for filling the DB.")
     parser.add_argument("--base-path", help="Path to use for finding the files. If omitted, use the current pwd.",
                         default=os.getcwd())
-    parser.add_argument("--no-replace", help="Do not replace the old database file.", action="store_true")
 
     args = parser.parse_args()
 
     if not os.path.exists("app.db"):
         shell.create_db()
-    elif not args.no_replace:
-        os.unlink("app.db")
-        shell.create_db()
+
+    base_path = args.base_path
 
     user = User.query.filter_by(name=args.user)
     if user.count() != 1:
@@ -29,65 +27,64 @@ if __name__ == '__main__':
 
     user = user.one()
 
-    base_path = args.base_path
-    top_level_entries = [os.path.join(base_path, dir) for dir in os.listdir(base_path)]
-    top_level_entries = filter(os.path.isdir, top_level_entries)
-
-    added_counter = 0
-
     new_label, _ = Label.get_or_add_label("new")
     probably_wrong_label, _ = Label.get_or_add_label("probably_wrong")
 
-    for possible_dance_dir in top_level_entries:
-        possible_dance_name = os.path.split(possible_dance_dir)[-1]
+    added_counter = 0
 
-        # Ask for permission
-        dance_name = possible_dance_name
-        dance_dir = possible_dance_dir
+    dance_replacement = {
+        "rb": "Rumba",
+        "sb": "Samba",
+        "qs": "Quickstep",
+        "df": "Disco Fox",
+        "cc": "Cha Cha",
+        "sf": "Slowfox",
+        "ww": "Wiener Walzer",
+        "ji": "Jive",
+        "tg": "Tango",
+        "lw": "Langsamer Walzer",
+    }
 
-        dance, dance_new_created = Dance.get_or_add_dance(dance_name)
+    for new_song_file in os.listdir(base_path):
+        # sb - 51 - Didi - Milk & Honey - (Vol. 37, Muevete!) - CM - PP,EN.mp3
+        splitted_song = new_song_file.split("-")
+        assert(len(splitted_song) == 7)
 
-        all_songs_with_this_dance = glob(os.path.join(dance_dir, "*/*.mp3"))
+        possible_dance_name = splitted_song[0].strip()
+        dance_name = dance_replacement.get(possible_dance_name, possible_dance_name)
+        dance, _ = Dance.get_or_add_dance(dance_name)
 
-        for file_name_with_this_dance in all_songs_with_this_dance:
-            dance_artist_title = os.path.splitext(os.path.split(file_name_with_this_dance)[-1])[0]
-            if dance_artist_title.count(" - ") != 2:
-                print("File name {} in wrong format. Skipping.".format(file_name_with_this_dance))
-                continue
-            else:
-                dance_name, artist_name, title_with_tag = dance_artist_title.split(" - ")
-                artist, artist_new_created = Artist.get_or_add_artist(artist_name)
+        bpm = int(splitted_song[1])
 
-                labels = []
+        title = splitted_song[2]
 
-                if "(" in title_with_tag and title_with_tag[-1] == ")":
-                    title = title_with_tag[:title_with_tag.rfind("(")]
-                    label_name_string = title_with_tag[title_with_tag.rfind("(") + 1:title_with_tag.rfind(")")]
-                    label_names = label_name_string.split(",")
-                    labels = [Label.get_or_add_label(label_name.strip())[0] for label_name in label_names]
-                else:
-                    title = title_with_tag
+        artist_name = splitted_song[3]
+        artist, _ = Artist.get_or_add_artist(artist_name)
 
-                if "_" in title or "_" in artist.name or "_" in dance.name:
-                    labels.append(probably_wrong_label)
+        album = splitted_song[5]
+        album_label, _ = Label.get_or_add_label(album)
 
-                new_song = Song()
-                new_song.title = title
-                new_song.artist = artist
-                new_song.dance = dance
-                new_song.creation_user = user
-                new_song.path = file_name_with_this_dance.replace(base_path, "")[1:]
-                new_song.duration = get_song_duration(file_name_with_this_dance)
+        labels = []
 
-                labels.append(new_label)
-                new_song.labels = labels
+        new_song = Song()
+        new_song.title = title
+        new_song.artist = artist
+        new_song.dance = dance
+        new_song.creation_user = user
+        new_song.bpm = bpm
+        new_song.path = os.path.join("Neue_Lieder/2019_12_01/", new_song_file)
+        new_song.duration = get_song_duration(os.path.join(base_path, new_song_file))
 
-                db.session.add(new_song)
+        labels.append(new_label)
+        labels.append(album_label)
+        new_song.labels = labels
 
-                added_counter += 1
+        db.session.add(new_song)
 
-            if added_counter % 10 == 0:
-                print("Added", added_counter, "song to the database")
+        added_counter += 1
 
-                db.session.commit()
+        if added_counter % 10 == 0:
+            print("Added", added_counter, "song to the database")
+
+    #db.session.commit()
 
